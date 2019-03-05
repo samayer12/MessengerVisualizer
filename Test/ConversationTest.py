f8ce0cbe3a388cae408c784bd1f409a2f1c6111c
Sam Mayer <samuel.a.mayer@outlook.com>
2019-03-05 09:59:00 -0600

    Create conversation class to represent JSON

---
+++ Test/ConversationTest.py
@@ -0,0 +1,40 @@
+import unittest
+from Conversation import Conversation
+from FileIO import FileIO
+
+skeleton_JSON = FileIO()
+skeleton_JSON.open_file('Messages\message_skeleton.json')
+conversation = Conversation(skeleton_JSON.data)
+
+
+class ConversationTest(unittest.TestCase):
+    def parse_participants(self):
+        self.assertEqual(conversation.participants, [{'name': 'Alice'}, {'name': 'Bob'}])
+
+    def parse_messages(self):
+        self.assertEqual(conversation.messages, [
+            {
+              "sender_name": "Alice",
+              "timestamp_ms": 1535232149475,
+              "content": "Hello, Bob",
+              "type": "Generic"
+            },
+            {
+              "sender_name": "Bob",
+              "timestamp_ms": 1535228809355,
+              "content": "Hello, Alice.",
+              "type": "Generic"
+            }
+          ])
+
+    def parse_title(self):
+        self.assertEqual(conversation.title, "Conversation")
+
+    def parse_is_still_participant(self):
+        self.assertEqual(conversation.is_still_participant, True)
+
+    def parse_thread_type(self):
+        self.assertEqual(conversation.thread_type, 'Regular')
+
+    def parse_thread_path(self):
+        self.assertEqual(conversation.thread_path, "Conversation_1337")
