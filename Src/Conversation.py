f25852fa658232841f82f95a99d3ee5bca6e1050
Sam Mayer <samuel.a.mayer@outlook.com>
2019-03-05 10:33:33 -0600

    Minor refactors

--- Src/Conversation.py
+++ Src/Conversation.py
@@ -1,5 +1,4 @@
 class Conversation:
-
     participants = ""
     messages = []
     title = ""
@@ -7,13 +6,10 @@ class Conversation:
     thread_type = ""
     thread_path = ""
 
-
     def __init__(self, conversation_source):
-        print("Processing conversation")
         self.participants = conversation_source["participants"]
         self.messages = conversation_source["messages"]
         self.title = conversation_source["title"]
         self.is_still_participant = conversation_source["is_still_participant"]
         self.thread_type = conversation_source["thread_type"]
-        self.thread_path = conversation_source["thread_path" ]
-
+        self.thread_path = conversation_source["thread_path"]
