3a63f597a52f2ffcc76df02b08c0695df01a1ed2
Sam Mayer <samuel.a.mayer@outlook.com>
2019-03-05 10:32:39 -0600

    Add message class to store individual messages

---
+++ Test/MessageTest.py
@@ -0,0 +1,18 @@
+import unittest
+from Message import Message
+from FileIO import FileIO
+
+skeleton_JSON = FileIO()
+skeleton_JSON.open_file('Messages\message_skeleton.json')
+message = Message(skeleton_JSON.data)
+
+
+class MessageTest(unittest.TestCase):
+    def parse_sender(self):
+        self.assertEqual(message.sender, "Alice")
+    def parse_timestamp(self):
+        self.assertEqual(message.timestamp, 1535232149475)
+    def parse_content(self):
+        self.assertEqual(message.content, "Hello, Bob")
+    def parse_type(self):
+        self.assertEqual(message.type, "Generic")
