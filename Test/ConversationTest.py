8edaac3c3dad79322022247c519372f5742ef097
Sam Mayer <samuel.a.mayer@outlook.com>
2019-03-05 20:28:47 -0600

    Rename & fix tests

--- Test/ConversationTest.py
+++ Test/ConversationTest.py
@@ -7,43 +7,56 @@ skeleton_JSON.open_file('Messages\conversation_skeleton.json')
 conversation = Conversation(skeleton_JSON.data)
 
 
+def del_none(data):
+    for entry in data:
+        data_dictionary = entry.__dict__
+        for key in data_dictionary.copy():
+            if data_dictionary[key] is (None or ''):
+                del data_dictionary[key]
+    return data
+
+
 class InitializationTest(unittest.TestCase):
-    def parse_participants(self):
+    def test_parse_participants(self):
         self.assertEqual(conversation.participants, ['Alice', 'Bob'])
 
-    def parse_messages(self):
-        messages_list = json.dumps(conversation.messages, default=lambda m: m.__dict__)
+    def test_parse_messages(self):
+        filtered_messages = del_none(conversation.messages)
+
+
+        messages_list = json.dumps(filtered_messages, default=lambda m: m.__dict__)
         messages_list = messages_list.replace("\"", "\'")
 
+        self.maxDiff=None
         self.assertEqual(messages_list, str((skeleton_JSON.data["messages"])))
 
-    def parse_title(self):
+    def test_parse_title(self):
         self.assertEqual(conversation.title, "Conversation")
 
-    def parse_is_still_participant(self):
+    def test_parse_is_still_participant(self):
         self.assertEqual(conversation.is_still_participant, True)
 
-    def parse_thread_type(self):
+    def test_parse_thread_type(self):
         self.assertEqual(conversation.thread_type, 'Regular')
 
-    def parse_thread_path(self):
+    def test_parse_thread_path(self):
         self.assertEqual(conversation.thread_path, "Conversation_1337")
 
 
 class ProcessingTest(unittest.TestCase):
-    def count_messages(self):
+    def test_count_messages(self):
         totals = conversation.get_message_totals()
 
-        self.assertEqual(totals["Alice"], 1)
-        self.assertEqual(totals["Bob"], 1)
+        self.assertEqual(totals["Alice"], 2)
+        self.assertEqual(totals["Bob"], 2)
 
-    def count_photos(self):
+    def test_count_photos(self):
         totals = conversation.get_photo_totals()
 
         self.assertEqual(totals["Alice"], 1)
         self.assertNotIn("Bob", totals)
 
-    def count_shares(self):
+    def test_count_shares(self):
         totals = conversation.get_share_totals()
 
         self.assertNotIn("Alice", totals)
--- Test/FileIOTest.py
+++ Test/FileIOTest.py
@@ -3,10 +3,10 @@ from FileIO import FileIO
 
 
 class FileIOTest(unittest.TestCase):
-    def FileIORuns(self):
+    def test_FileIORuns(self):
         self.assertEqual(FileIO.hello(self), "Hello")
 
-    def OpenSpecifiedFileInSubDir(self):
+    def test_OpenSpecifiedFileInSubDir(self):
         f = FileIO()
         self.assertEqual(f.open_file('Messages\Erin\message.json'), 'Messages\Erin\message.json')
         self.assertGreater(f.data.__len__(), 0)
