f064b4d82b68ec53508a735d94393ac0ed27aef8
Sam Mayer <samuel.a.mayer@outlook.com>
2019-03-06 11:24:36 -0600

    Add command-line processing

--- Src/Visualizer.py
+++ Src/Visualizer.py
@@ -1,11 +1,35 @@
+import sys, getopt, os
 from FileIO import FileIO
+from Conversation import Conversation
 
 
-def main():
-    print("Hello World!")
+def main(argv):
+    inputfile = ''
+    outputdir = ''
+
+    try:
+        opts, args = getopt.getopt(argv, "f:oh", ["ffile=", "odir="])
+    except getopt.GetoptError:
+        print('Error, check file paths and run with these options:\n'
+              'Visualizer.py -f <inputfile> -o <outputfile>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('Visualizer.py -f <inputfile> -o <outputdir>')
+            sys.exit()
+        elif opt in ("-f", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputdir = arg
+
     fileIO = FileIO()
+    conversation = Conversation(fileIO.open_file(inputfile))
+    # print(conversation.get_text())
+
+    print(conversation.get_text_by_sender())
+
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
 
 print("End")
