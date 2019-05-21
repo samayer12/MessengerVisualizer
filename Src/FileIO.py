2517cdcff2034e29f4ad3163a28c6f4d47b997b0
Sam Mayer <samuel.a.mayer@outlook.com>
2019-05-20 22:04:29 -0500

    Add ability to parse .txt files

--- Src/FileIO.py
+++ Src/FileIO.py
@@ -1,4 +1,4 @@
-import json
+import json, os
 
 
 class FileIO:
@@ -10,7 +10,20 @@ class FileIO:
     def hello(self):
         return "Hello"
 
-    def open_file(self, file):
+    def open_json(self, file):
         with open(file) as f:
             self.data = json.load(f)
         return self.data
+
+    def open_text(self, file):
+        try:
+            if file.endswith('.txt'):
+                text = ""
+                with open(os.path.join(os.getcwd(), file), 'r') as f:
+                    for line in f:
+                        if (line.startswith('#')):
+                           continue
+                        text += line
+                    return text
+        except TypeError:
+            raise TypeError('Invalid file extension. Must be .txt');
