a80826f6025cd298f69ee868b0d41e7d3654fd70
sam <samayer12@gmail.com>
2020-03-18 15:10:14 -0500

    Refactor test formats and file paths

--- test/test_conversation.py
+++ test/test_conversation.py
@@ -13,55 +13,79 @@ def del_none(data):
 
 
 class InitializationTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.skeleton_JSON = FileIO()
+        cls.skeleton_JSON.open_json('Messages/conversation_skeleton.json')
+        cls.conversation = Conversation(cls.skeleton_JSON.data)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.skeleton_JSON = None
+        cls.conversation = None
+
     def test_parse_participants(self):
-        self.assertEqual(conversation.participants, ['Alice', 'Bob'])
+        self.assertEqual(self.conversation.participants, ['Alice', 'Bob'])
 
     def test_parse_messages(self):
-        for msg in conversation.messages:
+        for msg in self.conversation.messages:
             # Kludge because I store timestamps as datetime, please forgive me
             msg.timestamp = int(msg.timestamp.timestamp() * 1000 - 18000000)
-        filtered_messages = del_none(conversation.messages)
+        filtered_messages = del_none(self.conversation.messages)
 
         messages_list = json.dumps(filtered_messages, default=lambda m: m.__dict__)
         messages_list = messages_list.replace("\"", "\'")
         messages_list = messages_list.replace("timestamp", "timestamp_ms")
 
         self.maxDiff = None
-        self.assertEqual(messages_list, str((skeleton_JSON.data["messages"])))
+        self.assertEqual(messages_list, str((self.skeleton_JSON.data["messages"])))
 
     def test_parse_title(self):
-        self.assertEqual(conversation.title, "Conversation")
+        self.assertEqual(self.conversation.title, "Conversation")
 
     def test_parse_is_still_participant(self):
-        self.assertEqual(conversation.is_still_participant, True)
+        self.assertEqual(self.conversation.is_still_participant, True)
 
     def test_parse_thread_type(self):
-        self.assertEqual(conversation.thread_type, 'Regular')
+        self.assertEqual(self.conversation.thread_type, 'Regular')
 
     def test_parse_thread_path(self):
-        self.assertEqual(conversation.thread_path, "Conversation_1337")
+        self.assertEqual(self.conversation.thread_path, "Conversation_1337")
+
 
 class ProcessingTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.skeleton_JSON = FileIO()
+        cls.skeleton_JSON.open_json('Messages/conversation_skeleton.json')
+        cls.conversation = Conversation(cls.skeleton_JSON.data)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.skeleton_JSON = None
+        cls.conversation = None
+
     def test_count_messages(self):
-        totals = conversation.get_message_totals()
+        totals = self.conversation.get_message_totals()
 
         self.assertEqual(totals["Alice"], 4)
         self.assertEqual(totals["Bob"], 3)
 
     def test_count_photos(self):
-        totals = conversation.get_photo_totals()
+        totals = self.conversation.get_photo_totals()
 
         self.assertEqual(totals["Alice"], 1)
         self.assertNotIn("Bob", totals)
 
     def test_count_shares(self):
-        totals = conversation.get_share_totals()
+        totals = self.conversation.get_share_totals()
 
         self.assertNotIn("Alice", totals)
         self.assertEqual(totals["Bob"], 1)
 
     def test_get_text(self):
-        raw_text = conversation.get_text()
+        raw_text = self.conversation.get_text()
 
         self.assertEqual(raw_text,
                          u"Hello, Bob\n"
@@ -72,11 +96,11 @@ class ProcessingTest(unittest.TestCase):
                          )
 
     def test_get_average_message_length(self):
-        avg_message = conversation.get_average_message_length(conversation.get_text())
+        avg_message = self.conversation.get_average_message_length(self.conversation.get_text())
         self.assertEqual(avg_message, 4.5)
 
     def test_prepare_all_messages(self):
-        raw_text = conversation.get_messages()
+        raw_text = self.conversation.get_messages()
 
         self.assertEqual(raw_text,
                          u"2019-08-24 02:32:23: Alice: Hello, Bob\n"
@@ -87,7 +111,7 @@ class ProcessingTest(unittest.TestCase):
                          )
 
     def test_get_messages_by_sender(self):
-        raw_text = conversation.get_messages_by_sender()
+        raw_text = self.conversation.get_messages_by_sender()
 
         self.assertEqual(raw_text,
                          {"Alice": u"2019-08-24 02:32:23: Hello, Bob\n2019-08-29 09:22:29: How are you?\n"
@@ -96,7 +120,7 @@ class ProcessingTest(unittest.TestCase):
                          )
 
     def test_get_message_count_by_type(self):
-        message_types = conversation.get_type_count()
+        message_types = self.conversation.get_type_count()
 
         self.assertEqual(message_types,
                          {"Content": 5,
@@ -105,7 +129,7 @@ class ProcessingTest(unittest.TestCase):
                           )
 
     def test_get_message_count_by_day(self):
-        message_counts = conversation.get_by_day()
+        message_counts = self.conversation.get_by_day()
 
         self.assertEqual(message_counts,
                          {"Monday": 1,
@@ -117,7 +141,7 @@ class ProcessingTest(unittest.TestCase):
                           "Sunday": 1})
 
     def test_get_message_count_by_hour(self):
-        message_counts = conversation.get_by_hour()
+        message_counts = self.conversation.get_by_hour()
 
         self.assertEqual(message_counts,
                          [(0, 0), (1, 0), (2, 1), (3, 0), (4, 0), (5, 0),
