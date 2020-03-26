b481299a61b6100c5198e37ed1085caad7cf7421
Sam Mayer <samayer12@gmail.com>
2020-03-26 16:59:14 -0500

    Format change

--- src/FileIO.py
+++ src/FileIO.py
@@ -8,20 +8,18 @@ class FileIO:
         self.data = ""
 
     def open_json(self, file):
-        print("JSON file IO starting\n ")
         with open(file) as f:
             self.data = json.load(f)
         return self.data
 
     def open_text(self, file):
-        print("Text file IO starting\n ")
         try:
             if file.endswith('.txt'):
                 text = ""
                 with open(os.path.join(os.getcwd(), file), 'r') as f:
                     for line in f:
-                        if (line.startswith('#')):
-                           continue
+                        if line.startswith('#'):
+                            continue
                         text += line
                     return text
         except TypeError:
