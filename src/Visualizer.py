c0ebc6d4479b67108f7ff2d19ac98c223014087b
Sam Mayer <samayer12@gmail.com>
2020-03-27 13:40:08 -0500

    Shuffle methods around

--- src/Visualizer.py
+++ src/Visualizer.py
@@ -2,13 +2,13 @@ import nltk
 import matplotlib.pyplot as plt
 
 
+def strip_common(words, wordlist):
+    return [word.lower() for word in words if word.lower() not in wordlist]
+
+
 class Visualizer:
     def __init__(self):
         pass
-
-    @staticmethod
-    def strip_common(words, wordlist):
-        return [word.lower() for word in words if word.lower() not in wordlist]
 
     @staticmethod
     def plot_frequency(data):
@@ -21,10 +21,11 @@ class Visualizer:
         plt.pie(data, labels=label)
         plt.show()
 
-    def plot_word_frequency(self, conversation, wordlist=None):
+    @staticmethod
+    def plot_word_frequency(conversation, wordlist=None):
         tokens = [t for t in conversation.split()]
         if wordlist is not None:
-            tokens = self.strip_common(tokens, wordlist)
+            tokens = strip_common(tokens, wordlist)
         freq = nltk.FreqDist(tokens)
         for key, val in freq.items():
             print(str(key) + ':' + str(val))
