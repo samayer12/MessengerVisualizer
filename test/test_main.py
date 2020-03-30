370db5bb4eefd92eedca3ef0aa56153de3827d94
Sam Mayer <samayer12@gmail.com>
2020-03-30 13:50:52 -0500

    Refactor test code

--- test/test_main.py
+++ test/test_main.py
@@ -4,10 +4,11 @@ import src.Main
 
 
 class MainTestCases(unittest.TestCase):
-    @patch('src.Visualizer.Visualizer.plot_frequency')
-    @patch('src.Visualizer.Visualizer.plot_word_frequency')
-    def test_plot_methods_called_correct_number_of_times(self, mock_word_frequency, mock_frequency):
-        src.Main.graphData("data")
+    @patch('src.Conversation')
+    @patch('src.Visualizer.Visualizer.plot_frequency', return_value="Graph1")
+    @patch('src.Visualizer.Visualizer.plot_word_frequency', return_value="Graph2")
+    def test_plot_methods_called_correct_number_of_times(self, mock_word_frequency, mock_frequency, mock_conversation):
+        src.Main.graphData(mock_conversation, wordlist=None)
         self.assertEqual(1, mock_word_frequency.call_count)
         self.assertEqual(2, mock_frequency.call_count)
 
