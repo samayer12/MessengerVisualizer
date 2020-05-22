cf4257923efd5f86271dab77c9abf0205e82a70f
Sam Mayer <samayer12@gmail.com>
2020-05-22 12:49:39 -0500

    Add reaction parsing

--- src/Message.py
+++ src/Message.py
@@ -1,5 +1,5 @@
 import datetime
-
+from collections import defaultdict
 
 class Message:
 
@@ -22,6 +22,38 @@ class Message:
             self.share = ""
             pass
         self.type = message_source["type"]
+        try:
+            self.reactions, self.unsupported_reactions = self.parse_reactions(message_source['reactions'])
+        except KeyError:
+            # There's no reaction, so why bother?
+            pass
+
+    def parse_reactions(self, reactions):
+        message_reactions = defaultdict(int)
+        unsupported_reactions = []
+        if reactions is None:
+            raise KeyError
+        for react in reactions:
+            current_reaction = react['reaction']
+            if current_reaction == "\u00f0\u009f\u0091\u008e":
+                message_reactions['Dislike'] += 1
+            elif current_reaction == "\u00f0\u009f\u0091\u008d":
+                message_reactions['Like'] += 1
+            elif current_reaction == "\u00f0\u009f\u0098\u00a0":
+                message_reactions['Angry'] += 1
+            elif current_reaction == "\u00f0\u009f\u0098\u00a2":
+                message_reactions['Sad'] += 1
+            elif current_reaction == "\u00f0\u009f\u0098\u00ae":
+                message_reactions['Wow'] += 1
+            elif current_reaction == "\u00f0\u009f\u0098\u0086":
+                message_reactions['Funny'] += 1
+            elif current_reaction == "\u00f0\u00e2\u009d\u00a4":
+                message_reactions['Love'] += 1
+            else:
+                message_reactions['Unsupported'] += 1
+                unsupported_reactions.append(current_reaction)
+
+        return message_reactions, unsupported_reactions
 
     def get_datetime(self):
         return self.timestamp.strftime('%Y-%m-%d %H:%M:%S')
--- test/test_message.py
+++ test/test_message.py
@@ -31,13 +31,6 @@ class MessageTest(unittest.TestCase):
     def test_parse_share(self):
         self.assertEqual(self.message.share, "")
 
-    def test_parse_like_reaction(self):
-        self.skeleton_JSON = FileIO()
-        self.skeleton_JSON.open_json('Messages/message_skeleton_reaction.json')
-        self.message = Message(self.skeleton_JSON.data)
-
-        self.assertEqual("like", self.reactions)
-
     def test_parse_type(self):
         self.assertEqual(self.message.type, "Generic")
 
@@ -45,6 +38,43 @@ class MessageTest(unittest.TestCase):
         datetime = '2018-08-25 16:22:29'
         self.assertEqual(datetime, self.message.get_datetime())
 
+
+class ReactionTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.skeleton_JSON = FileIO()
+        cls.skeleton_JSON.open_json('Messages/message_skeleton_reaction.json')
+        cls.message = Message(cls.skeleton_JSON.data)
+
+    @classmethod
+    def tearDownClass(cls):
+        del cls.skeleton_JSON
+        del cls.message
+
+    def test_parse_like_reaction(self):
+        self.assertEqual(1, self.message.reactions['Like'])
+
+    def test_parse_dislike_reaction(self):
+        self.assertEqual(1, self.message.reactions['Dislike'])
+
+    def test_parse_angry_reaction(self):
+        self.assertEqual(1, self.message.reactions['Angry'])
+
+    def test_parse_sad_reaction(self):
+        self.assertEqual(1, self.message.reactions['Sad'])
+
+    def test_parse_wow_reaction(self):
+        self.assertEqual(1, self.message.reactions['Wow'])
+
+    def test_parse_laughing_reaction(self):
+        self.assertEqual(1, self.message.reactions['Funny'])
+
+    def test_parse_love_reaction(self):
+        self.assertEqual(1, self.message.reactions['Love'])
+
+    def test_parse_unsupported_reaction(self):
+        self.assertEqual(1, self.message.reactions['Unsupported'])
+        self.assertEqual(["\u0078\u006b\u0063\u0064"], self.message.unsupported_reactions)
 
 if __name__ == '__main__':
     unittest.main()
