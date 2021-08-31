657bc3debf7d580e1192bb38d61be9aedb061c3c
Sam Mayer <samayer12@gmail.com>
2021-08-30 19:52:52 -0500

    Kludges to incorporate my images into the report

--- report_generator_project_files/report_generator_example.py
+++ report_generator_project_files/report_generator_example.py
@@ -5,39 +5,78 @@ import numpy as np
 def generate_report(dataset):
     report = report_util.Report("Random Number Dataset Report")
 
-    section = report.add_section("Dataset Stats")
+    section = report.add_section("Overview")
 
     paragraph = section.add_paragraph()
     mean = sum(dataset) / len(dataset)
     standard_deviation = dataset.std()
-
-    paragraph.append(f"The dataset has {len(dataset)} numbers. ")
-    paragraph.append(f"The largest number in the dataset is {max(dataset)}. ")
-    paragraph.append(f"The smallest number in the dataset is {min(dataset)}. ")
-    paragraph.append(f"The dataset average value is {mean} with a standard deviation of {standard_deviation}. ")
+    paragraph.append(
+        'This project proposal builds upon an existing project that visualizes Facebook Messenger data. '
+        'Source data comes from Facebook Messenger data that individuals may download from their own profile. '
+        'This data is passed to a command-line utility written in Python. '
+        'Reference: https://github.com/samayer12/MessengerVisualizer for the existing state of the project. '
+    )
 
     paragraph_2 = section.add_paragraph()
+
     ##########################################################################
-    # The following code demonstrates creating a figure directly with the matplotlib API
+    # Alice_Balance
     ##########################################################################
     figure_1 = section.add_figure()
-    figure_1.caption = "Dataset Histogram"
+    figure_1.caption = "Message Balance for Sender 'Alice'"
     # notice in the next line we access matplotlib's figure object directly
     ax = figure_1.matplotlib_figure.add_subplot(1, 1, 1)
     ax.hist(dataset)
-    ax.set_xlabel("Number Values")
-    ax.set_ylabel("Count")
-    figure_1.matplotlib_figure.tight_layout()
-
-    paragraph_2.append_cross_reference(figure_1)
+    ##########################################################################
+    # Bob_Balance
+    ##########################################################################
+    figure_2 = section.add_figure()
+    figure_2.caption = "Message Balance for Sender 'Bob'"
+    # notice in the next line we access matplotlib's figure object directly
+    ax = figure_2.matplotlib_figure.add_subplot(1, 1, 1)
+    ax.hist(dataset)
+    ##########################################################################
+    # Global Balance
+    ##########################################################################
+    figure_3 = section.add_figure()
+    figure_3.caption = "Message Balance for All Senders"
+    # notice in the next line we access matplotlib's figure object directly
+    ax = figure_3.matplotlib_figure.add_subplot(1, 1, 1)
+    ax.hist(dataset)
+    paragraph_2.append_cross_reference(figure_3)
     paragraph_2.append(f" shows the histogram distribution of the numbers in the dataset. ")
+
+    ##########################################################################
+    # Frequency_daily
+    ##########################################################################
+    figure_4 = section.add_figure()
+    figure_4.caption = "Message Frequency by Day of Week"
+    # notice in the next line we access matplotlib's figure object directly
+    ax = figure_4.matplotlib_figure.add_subplot(1, 1, 1)
+    ax.hist(dataset)
+    ##########################################################################
+    # Frequency_hourly
+    ##########################################################################
+    figure_5 = section.add_figure()
+    figure_5.caption = "Message Frequency by Hour of Day"
+    # notice in the next line we access matplotlib's figure object directly
+    ax = figure_5.matplotlib_figure.add_subplot(1, 1, 1)
+    ax.hist(dataset)
+    ##########################################################################
+    # Frequency_words
+    ##########################################################################
+    figure_6 = section.add_figure()
+    figure_6.caption = "Message Frequency by Word"
+    # notice in the next line we access matplotlib's figure object directly
+    ax = figure_6.matplotlib_figure.add_subplot(1, 1, 1)
+    ax.hist(dataset)
     ##########################################################################
 
     ##########################################################################
     # The following code demonstrates creating a table
     ##########################################################################
     tbl_1 = section.add_table()
-    tbl_1.caption = "Dataset Listing"
+    tbl_1.caption = "Message Listing"
 
     tbl_1.set_header(["Value", "Less Than 0.3", "Has digit '3'"])
     tbl_1.set_data(zip(dataset, [x < 0.3 for x in dataset], [str(x).find('3') > -1 for x in dataset]))
@@ -50,42 +89,6 @@ def generate_report(dataset):
     ##########################################################################
     section_2 = report.add_section("More Random Data")
     paragraph_3 = section_2.add_paragraph()
-
-    ##########################################################################
-    # The following code demonstrates creating a figure directly with the matplotlib API
-    ##########################################################################
-    figure_2 = section_2.add_figure()
-
-    # Generate some more example data for figure
-    dt = 0.01
-    t = np.arange(0, 30, dt)
-    nse1 = np.random.randn(len(t))  # white noise 1
-    nse2 = np.random.randn(len(t))  # white noise 2
-    s1 = np.sin(2 * np.pi * 10 * t) + nse1
-    s2 = np.sin(2 * np.pi * 10 * t) + nse2
-
-    # The following starts the plot creation using data generated above
-    # notice in the next line we access matplotlib's figure object directly
-    axs = [figure_2.matplotlib_figure.add_subplot(2, 1, 1), figure_2.matplotlib_figure.add_subplot(2, 1, 2)]
-    axs[0].plot(t, s1, t, s2)
-    axs[0].set_xlim(0, 2)
-    axs[0].set_xlabel('time')
-    axs[0].set_ylabel('s1 and s2')
-    axs[0].grid(True)
-
-    cxy, f = axs[1].cohere(s1, s2, 256, 1. / dt)
-    axs[1].set_ylabel('coherence')
-    figure_2.matplotlib_figure.tight_layout()
-    ##########################################################################
-
-    paragraph_3.append_cross_reference(figure_2)
-    paragraph_3.append(" shows a plot of more random numbers.")
-    n_mean = sum(nse1) / len(nse1)
-    if n_mean > mean:
-        paragraph_3.append("This dataset has a larger mean compared to the baseline dataset. ")
-    else:
-        paragraph_3.append("This dataset has a smaller mean compared to the baseline dataset. ")
-    paragraph_3.append(f"The mean is {n_mean}. ")
 
     return report
 
